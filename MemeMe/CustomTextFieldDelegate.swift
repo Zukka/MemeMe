29210eb8ef47ad47b4889f06ff7224afb09a2531
Zukka <info@zukkadev.net>
2017-05-23 18:02:22 +0200

    Add ImagePicker and custom textfield

---
+++ MemeMe/CustomTextFieldDelegate.swift
@@ -0,0 +1,13 @@
+//
+//  CustomTextFieldDelegate.swift
+//  MemeMe
+//
+//  Created by Alessandro Bellotti on 23/05/17.
+//  Copyright © 2017 Alessandro Bellotti. All rights reserved.
+//
+
+import UIKit
+
+class CustomTextFieldDelegate: NSObject {
+
+}
